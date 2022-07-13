ad989c7145daf32c0dbb573ad1b139d623d169ce
ameliss <m.cherfaoui99@gmail.com>
2022-07-13 15:54:46 +0200

    ppx

--- src/ppx_info.ml
+++ src/ppx_info.ml
@@ -4,25 +4,71 @@ open Parsetree
 open Ast_mapper
 open Ast_helper
 
-let build_annot () =
+let build_annot record =
   (* todo a completer *)
-  Attr.mk (Location.mknoloc "infos") (PStr [])
+  Attr.mk (Location.mknoloc "infos") (PStr [ Str.eval record ])
 
-(* let _compile infos =
- *   Exp.record *)
+let compile (infos : Collector.declaration_result) =
+  Exp.record
+    [
+      ( { txt = Lident "declaration_name"; loc = !default_loc },
+        Exp.constant
+          (Pconst_string (infos.declaration_name, !default_loc, None)) );
+      ( { txt = Lident "declaration_size"; loc = !default_loc },
+        Exp.constant
+          (Pconst_integer (string_of_int infos.declaration_size, None)) );
+      ( { txt = Lident "collected_fun_calls"; loc = !default_loc },
+        Exp.array
+          (List.map
+             (fun s -> Exp.constant (Pconst_integer (s, None)))
+             (Collector.StringSet.elements infos.collected_fun_calls)) );
+      ( { txt = Lident "collected_constants_integer"; loc = !default_loc },
+        Exp.array
+          (List.map
+             (fun s -> Exp.constant (Pconst_integer (s, None)))
+             (Collector.StringSet.elements infos.collected_constants_integer))
+      );
+      ( { txt = Lident "collected_constants_char"; loc = !default_loc },
+        Exp.array
+          (List.map
+             (fun s -> Exp.constant (Pconst_integer (s, None)))
+             (Collector.StringSet.elements infos.collected_constants_char)) );
+      ( { txt = Lident "collected_constants_string"; loc = !default_loc },
+        Exp.array
+          (List.map
+             (fun s -> Exp.constant (Pconst_integer (s, None)))
+             (Collector.StringSet.elements infos.collected_constants_string)) );
+      ( { txt = Lident "collected_constants_float"; loc = !default_loc },
+        Exp.array
+          (List.map
+             (fun s -> Exp.constant (Pconst_integer (s, None)))
+             (Collector.StringSet.elements infos.collected_constants_float)) );
+      ( { txt = Lident "potential_exception"; loc = !default_loc },
+        Exp.constant
+          (Pconst_string
+             (Bool.to_string infos.potential_exception, !default_loc, None)) );
+      ( { txt = Lident "potential_catch_exception"; loc = !default_loc },
+        Exp.constant
+          (Pconst_string
+             (Bool.to_string infos.potential_catch_exception, !default_loc, None))
+      );
+      ( { txt = Lident "declaration_changed"; loc = !default_loc },
+        Exp.constant
+          (Pconst_string
+             (Bool.to_string infos.declaration_changed, !default_loc, None)) );
+    ]
+    None
 
 (* actual mapper : Parsetree -> Parsetree *)
 let mapper =
   let handle_bind _mapper (bind : value_binding) =
-    (* let _infos = Collector.work_binding bind in
-       let record = compile infos in
-    *)
-    let record = () in
+    let infos = Collector.work_record bind in
+    let record = compile infos in
     { bind with pvb_attributes = build_annot record :: bind.pvb_attributes }
   in
   { default_mapper with value_binding = handle_bind }
 
 let () =
-  let open Migrate_parsetree in
-  Driver.register ~name:"ppx_info" ~args:[] Versions.ocaml_410
-    (fun _config _cookies -> mapper)
+   let open Migrate_parsetree in
+   Driver.register ~name:"ppx_info" ~args:[] Versions.ocaml_410
+     (fun _config _cookies -> mapper)
